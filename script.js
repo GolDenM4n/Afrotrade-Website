adff963102cfda3c63e01923eb081641a19b4f9b
BlackSh33p <91266126+BlackSh33palpha@users.noreply.github.com>
2025-08-28 21:10:29 +0200

    index/homepage added

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add simple interaction for buttons
+document.querySelector(".btn.primary").addEventListener("click", () => {
+  alert("More info will be available soon!");
+});
+
+document.querySelector(".btn.outline").addEventListener("click", () => {
+  alert("Contact page coming soon!");
+});
